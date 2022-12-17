cd90abb1e28e94840e0ec0831cd6b25aca3a671a
Lenard Frommelt <lenny09918050@gmail.com>
2022-12-17 12:48:44 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -1,9 +1,9 @@
 module.exports = {
     getDependencies: () => {
         return {
-            "dayjs": "^1.11.3",
-            "gridjs": "^5.0.2",
-            "gridjs-selection": "^4.0.0",
+            "dayjs": "^1.11.7",
+            "gridjs": "^5.1.0",
+            // "gridjs-selection": "^4.0.0",
 	        "thingy-byte-utils":"^0.0.7"
         }
     }
